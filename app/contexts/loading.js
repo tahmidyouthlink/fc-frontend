4e6d574a43b6c141ee661d17c41448de806d743f
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2024-12-05 14:02:04 +0600

    feat(layout,contexts,loader): fix frontend loader
    This commit fixes the loading spinner visibily issue. The changes include:
    - Update the root layout to include loading context provider.
    - Add loader component that displays the loading spinner
    
    Additionally, it modifies the loading context to remove unnecessary comments and ensures it's a client component.

--- app/contexts/loading.js
+++ app/contexts/loading.js
@@ -1,11 +1,6 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-  useTransition,
-} from "react";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+"use client";
+
+import { createContext, useContext, useState } from "react";
 
 const LoadingContext = createContext({
   isPageLoading: false,
@@ -13,37 +8,7 @@ const LoadingContext = createContext({
 });
 
 export const LoadingProvider = ({ children }) => {
-  // const router = useRouter();
-  // const pathname = usePathname();
-  // const searchParams = useSearchParams();
-  // const [isPending, startTransition] = useTransition();
   const [isPageLoading, setIsPageLoading] = useState(true);
-
-  // useEffect(() => {
-  //   startTransition(() => {
-  //     setIsPageLoading(false);
-  //   });
-  //   console.log("load ctx INSIDE");
-
-  //   return () => {
-  //     startTransition(() => {
-  //       setIsPageLoading(true);
-  //     });
-  //     console.log("load ctx RETURN");
-  //   };
-  // }, [pathname, searchParams]);
-
-  // useEffect(() => {
-  //   // Add event listeners to route changes and update the loading state accordingly
-  //   router.events.on("routeChangeStart", () => setIsPageLoading(true));
-  //   router.events.on("routeChangeComplete", () => setIsPageLoading(false));
-
-  //   // Clean up the event listeners when the component is unmounted
-  //   return () => {
-  //     router.events.off("routeChangeStart", () => setIsPageLoading(true));
-  //     router.events.off("routeChangeComplete", () => setIsPageLoading(false));
-  //   };
-  // }, []);
 
   return (
     <LoadingContext.Provider value={{ isPageLoading, setIsPageLoading }}>
