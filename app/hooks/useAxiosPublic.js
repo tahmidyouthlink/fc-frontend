5d83838c9f45b66d6a63d2561dd7b993a3516097
tahmidyouthlink <t.rahman@youthlink.tech>
2025-05-26 23:08:23 +0600

    removed localhost and add cloud run backend

--- app/hooks/useAxiosPublic.js
+++ app/hooks/useAxiosPublic.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
   // baseURL: 'https://fashion-commerce-backend.vercel.app',
-  // baseURL: 'https://fc-backend-664306765395.asia-south1.run.app',
+  baseURL: 'https://fc-backend-664306765395.asia-south1.run.app',
 })
 
 const useAxiosPublic = () => {
