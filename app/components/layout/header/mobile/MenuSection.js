8244a7af45c814ee8f4bd1245a96e08c8d4acfed
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2025-06-15 11:13:18 +0600

    style(nav-menu): remove "Menu" heading
    This commit removes the "Menu" heading and its associated horizontal rule from the mobile navigation drawer. This change streamlines the mobile menu's visual presentation, making it cleaner and more concise, as per design requirements.
    
    The changes include:
    - Remove the `h5` element with the "MENU" text and the `hr` element directly below it from MenuSection component.
    - Adjust the parent `div` wrapper for the menu items in MenuSection component to directly render the `ul` element, as the `h5` and `hr` are no longer present.

--- app/components/layout/header/mobile/MenuSection.js
+++ app/components/layout/header/mobile/MenuSection.js
@@ -7,50 +7,46 @@ export default function MenuSection({ setIsNavMenuOpen }) {
   const searchParams = useSearchParams();
 
   return (
-    <div>
-      <h5 className="text-[10px] font-semibold text-neutral-500">MENU</h5>
-      <hr className="my-2 h-0.5 w-full bg-neutral-100" />
-      <ul className="space-y-1 text-xs md:text-[13px]">
-        <li>
-          <TransitionLink
-            href="/our-story"
-            hasDrawer={true}
-            setIsDrawerOpen={setIsNavMenuOpen}
-            className={pathname.startsWith("/our-story") ? "active" : undefined}
-          >
-            <PiUsersThree />
-            Souls
-          </TransitionLink>
-        </li>
-        <li>
-          <TransitionLink
-            href="/shop?filterBy=New+Arrivals"
-            hasDrawer={true}
-            setIsDrawerOpen={setIsNavMenuOpen}
-            className={
-              searchParams.get("filterBy") === "New Arrivals" ? "active" : ""
-            }
-          >
-            <PiGift />
-            Buzz
-          </TransitionLink>
-        </li>
-        <li>
-          <TransitionLink
-            href="/shop"
-            hasDrawer={true}
-            setIsDrawerOpen={setIsNavMenuOpen}
-            className={
-              pathname.startsWith("/shop") && !searchParams.get("filterBy")
-                ? "active"
-                : undefined
-            }
-          >
-            <PiBagSimple />
-            Threadz
-          </TransitionLink>
-        </li>
-      </ul>
-    </div>
+    <ul className="space-y-1 text-xs md:text-[13px]">
+      <li>
+        <TransitionLink
+          href="/our-story"
+          hasDrawer={true}
+          setIsDrawerOpen={setIsNavMenuOpen}
+          className={pathname.startsWith("/our-story") ? "active" : undefined}
+        >
+          <PiUsersThree />
+          Souls
+        </TransitionLink>
+      </li>
+      <li>
+        <TransitionLink
+          href="/shop?filterBy=New+Arrivals"
+          hasDrawer={true}
+          setIsDrawerOpen={setIsNavMenuOpen}
+          className={
+            searchParams.get("filterBy") === "New Arrivals" ? "active" : ""
+          }
+        >
+          <PiGift />
+          Buzz
+        </TransitionLink>
+      </li>
+      <li>
+        <TransitionLink
+          href="/shop"
+          hasDrawer={true}
+          setIsDrawerOpen={setIsNavMenuOpen}
+          className={
+            pathname.startsWith("/shop") && !searchParams.get("filterBy")
+              ? "active"
+              : undefined
+          }
+        >
+          <PiBagSimple />
+          Threadz
+        </TransitionLink>
+      </li>
+    </ul>
   );
 }
