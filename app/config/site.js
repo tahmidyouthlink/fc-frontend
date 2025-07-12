e5f066ae023cedb699adf4ab495bb7860f9318e4
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2025-07-12 16:38:17 +0600

    chore(config): update frontend URL to official domain
    This commit updates the `FRONTEND_URL` in `app/config/site.js` to the newly acquired official domain, `https://poshax.shop`, replacing the temporary one.

--- app/config/site.js
+++ app/config/site.js
@@ -2,6 +2,5 @@ export const WEBSITE_NAME = "PoshaX";
 export const BACKEND_URL =
   "https://fc-backend-664306765395.asia-south1.run.app";
 // export const BACKEND_URL = "http://localhost:5000";
-export const FRONTEND_URL =
-  "https://fc-frontend-664306765395.asia-south1.run.app";
+export const FRONTEND_URL = "https://poshax.shop";
 // export const FRONTEND_URL = "http://localhost:3000";
