2af856505fd8fc87ef1040a3a1dde04a902bea4d
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2025-07-15 23:25:23 +0600

    chore(config/site): update backend URL
    This commit updates the backend URL in `site.js` to the new deployment address.

--- app/config/site.js
+++ app/config/site.js
@@ -1,6 +1,6 @@
 export const WEBSITE_NAME = "PoshaX";
 export const BACKEND_URL =
-  "https://fc-backend-664306765395.asia-south1.run.app";
+  "https://poshax-backend-664306765395.asia-southeast1.run.app";
 // export const BACKEND_URL = "http://localhost:5000";
 export const FRONTEND_URL = "https://poshax.shop";
 // export const FRONTEND_URL = "http://localhost:3000";
