ece67701386c814d0cffaa543afcdae37919af08
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2025-08-03 19:01:21 +0600

    fix(tracking-code): disable copy functionality when unavailable
    This commit fixes an issue in the `TrackingCode` component where the copy-to-clipboard functionality was active even when no tracking code was provided.
    
    The changes include:
    - Render the copy icon and make the tracking code clickable only when `trackingCode` is a truthy value.
    - Display a static "--" text when `trackingCode` is undefined or null, without the copy functionality.

--- app/components/order/TrackingCode.js
+++ app/components/order/TrackingCode.js
@@ -16,13 +16,17 @@ export default function TrackingCode({ trackingCode }) {
   return (
     <div>
       <h4 className="text-nowrap font-semibold">Tracking Code</h4>
-      <span
-        className="flex cursor-pointer items-center gap-1 hover:text-neutral-800 sm:gap-2 [&>*]:transition-[color] [&>*]:duration-300 [&>*]:ease-in-out"
-        onClick={handleCopyToClipboard}
-      >
-        {trackingCode && <LuCopy className="" size={16} />}
-        <p className="text-nowrap text-right">{trackingCode || "--"}</p>
-      </span>
+      {!trackingCode ? (
+        <p className="text-nowrap text-right">{"--"}</p>
+      ) : (
+        <span
+          className="flex cursor-pointer items-center gap-1 hover:text-neutral-800 sm:gap-2 [&>*]:transition-[color] [&>*]:duration-300 [&>*]:ease-in-out"
+          onClick={handleCopyToClipboard}
+        >
+          <LuCopy className="" size={16} />
+          <p className="text-nowrap text-right">{trackingCode}</p>
+        </span>
+      )}
     </div>
   );
 }
